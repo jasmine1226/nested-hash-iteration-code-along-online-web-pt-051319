f710d620d0fe0ffdd857cc0d054e5ecbedd5dc5c
Jasmine Chen <jasmine1226@gmail.com>
2019-05-17 03:19:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, info|
+    info.each do |attribute, data|
+      data.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end
+  end
 end
